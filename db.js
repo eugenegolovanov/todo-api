be88a7152f1d6817de3135c7c1f63e3257eb8031
Eugene Golovanov <eugene.golovanov@hart.com>
2016-07-07 21:12:52 -0700

    postgress added

--- db.js
+++ db.js
@@ -1,8 +1,23 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect':'sqlite',
-	'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development'; //environment variable for heroku, NODE_ENV - heroku environment if it is set than it is production
+var sequelize;
+
+if (env === 'production') {
+	//Setup postgress for heroku, for production
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgress'
+	});
+} else {
+	//Setup for local
+	var sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect':'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
+}
+
+
+
+
 
 var db = {};
 
