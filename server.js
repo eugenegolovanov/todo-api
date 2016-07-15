24c6eacf2aae9b3f167bc0bedf0cd725b4ebfabe
Eugene Golovanov <eugene.golovanov@hart.com>
2016-07-15 00:39:06 -0700

    final

--- server.js
+++ server.js
@@ -18,59 +18,10 @@ app.get('/', function (req, res) {
 });
 
 
-// //GET all todos  /todos 
-// app.get('/todos', function (req, res) {
-// 	res.json(todos);//response as json no need to stringify
-// });
-
 
 //GET all todos or filtered  /todos?completed=false&q=haircut
 app.get('/todos', middleware.requireAuthentication, function (req, res) {
 
-	// var queryParams = req.query;//req.query give us string not boolean,
-	// var filteredTodos = todos;
-
-	// //Work With q Query    /todos?completed=false
-	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-	//  	 filteredTodos = _.where(todos, {completed: false});//where finds all matching items not just the first 
-	// } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-	//      filteredTodos = _.where(todos, {completed: true});//where finds all matching items not just the first 
-	// }
-
-
-	// //Work With 'q' Query    /todos?q=something
-	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-
-		// console.log('--------------------------------------------------------------------');
-		// console.log('Original filtered todos:');
-		// console.log(filteredTodos);
-		// console.log('--------------------------------------------------------------------');
-
-
-	// 	//Filter all todos and find if description containts queryParams.q
-	// 	var filteredTodos = _.filter(filteredTodos, function(todo){ 
-
-	// 		// var arrayOfWords = todo.description.split(" ");//Make description as array of words
-	// 		// return (arrayOfWords.indexOf(queryParams.q)) > -1;//if -1 queryParams.q doesn't exists  
-
-	// 		//If 'todo.desctiption' contains  'queryParams.q'
-	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;//REFACTORING
-
-	// 	});
-		
-
-		// console.log('--------------------------------------------------------------------');
-		// console.log('QUERY:');
-		// console.log(queryParams.q);
-
-		// console.log('FILTERED WITH QUERY:');
-		// console.log(filteredTodos);
-		// console.log('--------------------------------------------------------------------');
-
-
-	// }
-
-	// res.json(filteredTodos);//response as json no need to stringify
 
 
 
@@ -129,28 +80,6 @@ app.get('/todos', middleware.requireAuthentication, function (req, res) {
 //POST todo
 app.post('/todos', middleware.requireAuthentication, function (req, res) {
 
-	// //req.body - Body requested
-	// //_.pick - filter body with 'description' and 'completed' properties
-	// var body = _.pick(req.body, 'description', 'completed');
-	// console.log('Body requested:');
-	// console.log(body);
-
-	// //Check body if properties are valid types
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).res();//400 - user provided bad data
-	// }
-	// idCounter++;
-
-	// //Trim spaces if body has some
-	// body.description = body.description.trim()
-
-	// body.id = idCounter;
-
-	// todos.push(body);
-
-	// res.json(body);
-
-
 ////////////WITH DATABASE REFACTOR////////////////
 	//req.body - Body requested
 	//_.pick - filter body with 'description' and 'completed' properties
@@ -176,19 +105,6 @@ app.post('/todos', middleware.requireAuthentication, function (req, res) {
 
 //GET todos by id   /todos/:id
 app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
-
-	// var requestedId = parseInt(req.params.id, 10); //parseInt converts string to Int
-
-	// //get requested todo object (refactored with underscore library)
-	// var matchedTodo = _.findWhere(todos, {id: requestedId});//findWhere finds matching items 
-
-	// if (matchedTodo) {
-	// 	res.json(matchedTodo);
-	// } else {
-	// 	res.status(404).send();
-	// }
-
-////////////WITH DATABASE REFACTOR////////////////
 
 	var requestedId = parseInt(req.params.id, 10); //parseInt converts string to Int
 
@@ -221,25 +137,6 @@ app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
 
 //DELETE todos by id   /todos/:id
 app.delete('/todos/:id' , middleware.requireAuthentication, function (req, res) {
-	// var requestedId = parseInt(req.params.id, 10); //parseInt converts string to Int
-
-	// //get requested todo object (refactored with underscore library)
-	// var todoToDelete = _.findWhere(todos, {id: requestedId});
-
-	// console.log('---------------------OLD TODOS:--------------------------------');
-	// console.log(todos);
-	// console.log('----------------------NEW TODOS:-------------------------------');
-	// todos = _.without(todos, todoToDelete);
-	// console.log(todos);
-	// console.log('----------------------------------------------------------------');
-
-	// if (todoToDelete) {
-	// 	res.json(todoToDelete);
-	// } else {
-	// 	res.status(404).json({"error":"no todo with matced id"});
-	// }
-
-////////////WITH DATABASE REFACTOR////////////////
 
 	var requestedId = parseInt(req.params.id, 10); //parseInt converts string to Int
 
@@ -269,45 +166,6 @@ app.delete('/todos/:id' , middleware.requireAuthentication, function (req, res) 
 
 //PUT todos by id /todos/:id,  (update todos)
 app.put('/todos/:id', middleware.requireAuthentication, function (req, res) {
-	// //req.body - Body requested
-	// //_.pick - filter body with 'description' and 'completed' properties
-	// var body = _.pick(req.body, 'description', 'completed');
-	// var requestedId = parseInt(req.params.id, 10); //parseInt converts string to Int
-	// var matchedTodo = _.findWhere(todos, {id: requestedId});
-
-
-	// if (!matchedTodo) {
-	// 	res.status(404).send();
-	// } 
-
-	// var validAttributes = {};
-
-	// //completed attribute
-	// if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-	// 	//success, user provided attribute and it is boolean
-	// 	validAttributes.completed = body.completed;
-
-	// } else if (body.hasOwnProperty('completed')) {
-	// 	//error, user provided something inside 'completed' but it is not boolean
-	// 	res.status(400).send();
-	// }
-
-	// //description attribute
-	// if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-	// 	//success, user provided attribute and it is string
-	// 	validAttributes.description = body.description.trim();
-
-	// } else if (body.hasOwnProperty('description')) {
-	// 	//error, user provided something inside 'description' but it is not string
-	// 	res.status(400).send();
-	// }
-
-	// //IF WE HERE everything went successfully
-
-	// // matchedTodo.completed = validAttributes.completed;
-	// // matchedTodo.description = validAttributes.description;
-	// _.extend(matchedTodo, validAttributes);//underscore refactor, copy properties _.extend(destination, *sources)
-	// res.json(matchedTodo);
 
 	////////////WITH DATABASE REFACTOR////////////////
 
@@ -392,24 +250,6 @@ app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 	var userInstance;
 
-	// //All functionality is in user.js file in  'authenticate' method
-	// db.user.authenticate(body).then(function (user) {
-
-	// 	var token = user.generateToken('authentication');
-
-	// 	if (token) {
-	// 		res.header('Auth', token).json(user.toPublicJSON());	
-	// 	} else {
-	// 		res.status(401).send();
-	// 	}
-
-	// }, function () {
-	// 	res.status(401).send();
-	// });
-
-
-
-
 	//All functionality is in user.js file in  'authenticate' method
 	db.user.authenticate(body).then(function (user) {
 
@@ -443,7 +283,6 @@ app.delete('/users/login', middleware.requireAuthentication, function (req, res)
 	});
 
 });
-
 
 
 
